2a86d10a33fc1c2ccab2c33169c4525abb7285b5
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-06-18 14:46:28 -0400

    added ccbyt

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -1,4 +1,5 @@
 import memoryscapeImg from './img/memoryscape.png';
+import ccbytImg from './img/ccbyt.gif';
 
 export const PTypes = {
     UNITY: 'unity',
@@ -22,18 +23,25 @@ export const projects = [
         ],
         technologies: 'Unity, VR, C#, Firebase, Express.js, Node.js, JavaScript',
     },
+
     {
-        title: 'MemoryScape',
-        timeFrame: 'Mar 2023 - Present',
-        subtitle: 'DALI Lab, Dartmouth College',
+        title: 'Cookie Clicker but You Type',
+        timeFrame: 'August 2020 - Sept 2022',
+        subtitle: 'Personal Project, Steam Game',
         type: PTypes.UNITY,
-        description: 'MemoryScape is a VR application that allows users to create and share memories in a virtual space. Users can create memories by placing objects in a virtual space and attaching a memory to that object. Users can then share their memories with others by sending them a link to their virtual space. The application is built using Unity and C# for the front-end and Firebase, Express.js, and Node.js for the back-end. I am currently working on the front-end of the application, specifically the UI and UX of the application.',
-        image: memoryscapeImg,
-        links: [{
-            text: 'Contact For Repos',
-            link: 'mailto:devonstarr123@gmail.com',
-        }],
-        technologies: 'Unity, VR, C#, Firebase, Express.js, Node.js, JavaScript',
+        description: 'An incremental strategy-based typing game developed in Unity. Type words to earn keys, a currency which can be used to buy upgrades to earn keys faster. This addicting nature of progression helps hundreds of typists enjoy honing their skills.',
+        image: ccbytImg,
+        links: [
+            {
+                text: 'Steam Page',
+                link: 'https://store.steampowered.com/app/2102810/Cookie_Clicker_but_You_Type',
+            },
+            {
+                text: 'Contact For Repos',
+                link: 'mailto:devonstarr123@gmail.com',
+            },
+        ],
+        technologies: 'Unity, C#,',
     },
 
 ];
