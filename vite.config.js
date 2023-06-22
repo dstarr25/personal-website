211eac042032b90cc6177c99acd129d232ad1bb9
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-06-22 03:01:20 -0400

    added favicons back

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
-// import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
@@ -8,14 +8,9 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            'src/img': path.resolve(__dirname, 'src/img'),
-        },
-    },
     plugins: [
         eslint(),
-        // vitePluginFaviconsInject('./src/img/devonEmoji.png'),
+        vitePluginFaviconsInject('./src/img/devonEmoji.png'),
     ],
     css: {
         postcss: {
