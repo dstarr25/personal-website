171189597ce6664d370d84db5a19ad79bad64f7a
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-11-17 17:32:48 -0500

    some more projects and skills

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence, useAnimation, useInView } from 'framer-motion';
-import { TbBrandCSharp, TbBrandCpp, TbBrandCss3, TbBrandFirebase, TbBrandGit, TbBrandHtml5, TbBrandJavascript, TbBrandMongodb, TbBrandPython, TbBrandReact, TbBrandReactNative, TbBrandRedux, TbBrandUnity } from 'react-icons/tb';
+import { TbBrandCSharp, TbBrandCpp, TbBrandCss3, TbBrandFirebase, TbBrandGit, TbBrandHtml5, TbBrandJavascript, TbBrandMongodb, TbBrandMysql, TbBrandPython, TbBrandReact, TbBrandReactNative, TbBrandRedux, TbBrandTailwind, TbBrandTypescript, TbBrandUnity, TbBrandVue } from 'react-icons/tb';
 import { FaJava } from 'react-icons/fa';
 import Icon from './icon';
 import ViewDiv from './viewDiv';
@@ -29,22 +29,6 @@ const useWindowDimensions = () => {
 
     return windowDimensions;
 };
-
-const icons = [
-    'redux',
-    'html',
-    'css',
-    'react & react native',
-    'unity',
-    'c#',
-    'python',
-    'c++',
-    'javascript',
-    'firebase',
-    'mongodb',
-    'git',
-    'java',
-];
 
 function About() {
     const iconColor = '#F07F75';
@@ -115,14 +99,18 @@ function About() {
                         <Icon icon={<TbBrandRedux />} handleHover={handleHover} name="redux" size={skillSize} hoverColor="#764abc" initialColor={iconColor} />
                         <Icon icon={<TbBrandHtml5 />} handleHover={handleHover} name="html" size={skillSize} hoverColor="#DD4B24" initialColor={iconColor} />
                         <Icon icon={<TbBrandCss3 />} handleHover={handleHover} name="css" size={skillSize} hoverColor="#0068BA" initialColor={iconColor} />
+                        <Icon icon={<TbBrandTailwind />} handleHover={handleHover} name="tailwindcss" size={skillSize} hoverColor="#3EBFF8" initialColor={iconColor} />
                         <Icon icon={<TbBrandReactNative />} handleHover={handleHover} name="react & react native" size={skillSize} hoverColor="#5ED3F3" initialColor={iconColor} />
+                        <Icon icon={<TbBrandVue />} handleHover={handleHover} name="vue.js" size={skillSize} hoverColor="#41B883" initialColor={iconColor} />
                         <Icon icon={<TbBrandUnity />} handleHover={handleHover} name="unity" size={skillSize} hoverColor="#000000" initialColor={iconColor} />
                         <Icon icon={<TbBrandCSharp />} handleHover={handleHover} name="c#" size={skillSize} hoverColor="#764abc" initialColor={iconColor} />
                         <Icon icon={<TbBrandPython />} handleHover={handleHover} name="python" size={skillSize} hoverColor="#F7CC40" initialColor={iconColor} />
                         <Icon icon={<TbBrandCpp />} handleHover={handleHover} name="c++" size={skillSize} hoverColor="#0076F7" initialColor={iconColor} />
                         <Icon icon={<TbBrandJavascript />} handleHover={handleHover} name="javascript" size={skillSize} hoverColor="#E8D44D" initialColor={iconColor} />
+                        <Icon icon={<TbBrandTypescript />} handleHover={handleHover} name="typescript" size={skillSize} hoverColor="#087ECE" initialColor={iconColor} />
                         <Icon icon={<TbBrandFirebase />} handleHover={handleHover} name="firebase" size={skillSize} hoverColor="#F79B01" initialColor={iconColor} />
                         <Icon icon={<TbBrandMongodb />} handleHover={handleHover} name="mongodb" size={skillSize} hoverColor="#00E15F" initialColor={iconColor} />
+                        <Icon icon={<TbBrandMysql />} handleHover={handleHover} name="sql/mysql" size={skillSize} hoverColor="#FFA518" initialColor={iconColor} />
                         <Icon icon={<TbBrandGit />} handleHover={handleHover} name="git" size={skillSize} hoverColor="#E84D31" initialColor={iconColor} />
                         <Icon icon={<FaJava />} handleHover={handleHover} name="java" size={skillSize} hoverColor="#F0931C" initialColor={iconColor} />
                     </div>
